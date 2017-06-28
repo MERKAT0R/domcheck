cc902d592c4b3a7c9d5b40843c52e685e15ebf59
MERKATOR <merkator@merkator.pro>
2017-06-28 14:18:10 +0300

    no message

--- whois.php
+++ whois.php
@@ -1,5 +1,5 @@
 <?php
-//By MERKATOR <merkator@merkator.pro> 28.0.6.2017
+//By MERKATOR <merkator@merkator.pro> 28.06.2017
 include 'vendor/phois/whois/src/Phois/Whois/Whois.php';
 
 
@@ -9,12 +9,11 @@ $dom = range(0000, 9999);            							  //checks all domains in 0000..9999
 	
 $zones = array('com','net','org','ru','nu');  	  				  //List of zones
 
-//badcod starts here ;) do not modify below this line.
+//badcode starts here ;) do not modify below this line.
 foreach($dom as $d0m){ 	
 foreach($zones as $zone){	
 	$sld = "$d0m.$zone";
 	$domain = new Phois\Whois\Whois($sld);
-	$whois_answer = $domain->info();
 	if ($domain->isAvailable()) {
 		echo "DOMAIN FREE >> ".$domain->getDomain()."  \n";       //  If domain free it`l show at console - "DOMAIN FREE >> %DOMAINNAME%"
 	} 
